72056ae6a2b1076d4e09a75e7a3150bbeb7a3e4b
Stone_Choppa <kirillmajer2007@gmail.com>
2022-05-25 11:39:28 +0700

    lu

---
+++ app/src/main/java/com/example/btservice/Information.java
@@ -0,0 +1,14 @@
+package com.example.btservice;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Information extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
